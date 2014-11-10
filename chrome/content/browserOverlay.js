8be46c26e5756a23e9a9d795e1a0939fb19350bd
olivier DREVET <odrevet@gmail.com>
2014-11-10 17:01:54 +0100

    replace setAttribute with addEventListener; change how modules are listed (now in a javascript array)

--- chrome/content/browserOverlay.js
+++ chrome/content/browserOverlay.js
@@ -3,6 +3,7 @@ if("undefined" == typeof(vwofChrome)){
 }
 
 vwofChrome.BrowserOverlay = {
+    parser_name:["blip", "dailymotion", "FC2", "HTML5", "niconico", "youtube"],
     parsers:{},   //hash of the parsers (loaded from jsm modules)
 
     /**
@@ -10,8 +11,27 @@ vwofChrome.BrowserOverlay = {
     */
     startup:function() {
 	this.load_modules();
+	this.set_parsers_activation();
     },
+    set_parsers_activation:function(){
+	//check in the preferences : activated a parser on page load or not
+	Components.utils.import("resource://gre/modules/Services.jsm");
+	var prefManager = Components.classes["@mozilla.org/preferences-service;1"]
+	    .getService(Components.interfaces.nsIPrefBranch);
 
+	var modules_list = prefManager.getCharPref("extensions.vwof.modules");
+	var modules = JSON.parse(modules_list);  //parser / value from the preferences
+
+	for(var key_parser in this.parsers){
+ 	    var parser = this.parsers[key_parser].parser;
+
+	    //if a pref is defined for this parser
+	    if(modules[key_parser])
+		parser['activated'] = modules[key_parser]?true:false;
+	    else 
+		parser['activated'] = false;
+	}
+    },
     /**
        Load modules listed in the extensions.vwof.modules pref variable to this.parsers hash
     */
@@ -22,38 +42,19 @@ vwofChrome.BrowserOverlay = {
 	    var modules_list = prefManager.getCharPref("extensions.vwof.modules");
 	    var modules = JSON.parse(modules_list);
 
-	    for(var key_parser in modules){
-		if(modules[key_parser] == 1){
-		    let context = {};
-		    let res = 'resource://vwof/'+key_parser+'.jsm';
-		    Services.scriptloader.loadSubScript(res, context, "UTF-8");
-		    this.parsers[key_parser] = context;
-		}
+	    for(var i=0;i<this.parser_name.length;i++){
+		let key_parser = this.parser_name[i];
+		let context = {};
+		let res = 'resource://vwof/'+key_parser+'.jsm';
+		Services.scriptloader.loadSubScript(res, context, "UTF-8");
+		this.parsers[key_parser] = context;
 	    }
 	}
 	catch(err){
 	    alert(err);
 	};
 	
-    },
-
-    reload_modules:function(){
-	try{	    
-	    // clear the previously loaded parsers
-	    delete this.parsers;
-	    this.parsers = {};
-
-	    //clear the cache from where the resources are loaded
-	    Services.obs.notifyObservers(null, "startupcache-invalidate", null)
-
-	    //finally load the modules
-	    this.load_modules();
-	}
-	catch(err){
-	    alert(err);
-	};	
-    },
-    
+    },    
     /**
        call every parse function from loaded parsers
     */
@@ -66,6 +67,8 @@ vwofChrome.BrowserOverlay = {
   	    try{
 		var parser = this.parsers[key_parser].parser;
 		var video_data = [];  //array of video links with quality
+
+		if(parser['activated'] != true)continue;
 
 		//if the parser has a URI and it's the current location
 		if(parser.BASE_URI && cw.location.hostname == parser.BASE_URI){
@@ -105,9 +108,8 @@ vwofChrome.BrowserOverlay = {
     detectVideo:function(cw) {
 
 	//check if the content window is defined
-	if (!cw
-	    //|| cw.frameElement
-	    || !cw.document
+	if (!cw ||
+	    !cw.document
 	   ){
 	    return;
 	}
@@ -117,13 +119,9 @@ vwofChrome.BrowserOverlay = {
 	for (var i = 0; i < video_info.length; i++) {
 	    if(video_info[i]['player']){
     		var replace_location = video_info[i]['player'];
-		var player = vwofPlayer.create_player(video_info[i], cw);
+		var player = vwofPlayer.create_video_selector(video_info[i], cw);
 		var replace_parent = replace_location.parentNode;
 		replace_parent.replaceChild(player, replace_location);
-	    }
-	    else{
-		var j = vwofPlayer.find_prefered_video(video_info[i].videos);
-		gBrowser.selectedTab = gBrowser.addTab(video_info[i].videos[j].url);
 	    }
 	}
     }
@@ -158,7 +156,7 @@ var myPrefObserver = {
     observe: function(aSubject, aTopic, aData) {
 	switch (aData) {
 	case "modules":
-	    vwofChrome.BrowserOverlay.reload_modules();
+	    vwofChrome.BrowserOverlay.set_parsers_activation();
 	    break;
 	}
     }
--- chrome/content/player.js
+++ chrome/content/player.js
@@ -6,7 +6,6 @@ vwofPlayer = {
     /**
        add the player stylecheet link to the head of the document
     */
-    
     add_style:function(doc, style_id){
 	var style = doc.createElement('link');
 	style.setAttribute('type', 'text/css');
@@ -15,19 +14,49 @@ vwofPlayer = {
 	style.setAttribute('id', style_id);
 	doc.head.appendChild(style);
     },
-    
     /**
-       Create the html code to embed in the document
+       callback function called when the video selector is clicked
+       replace the video selector with a frame pointing to the selected video
+       firefox will set the content of the frame with it's HTML5 video player
     */
-    create_player:function(video_info, cw){
+    video_selector_click:function(event){
+	const doc = event.originalTarget.ownerDocument;
+        var player_style_size = 'min-width:'+this.clientWidth+'px;min-height:'+this.clientHeight+'px;';
+
+	var i = document.createElement('iframe');
+	i.setAttribute('id', 'vwof_frame_'+this.id);
+	i.setAttribute('class', 'vwof_frame');
+	i.setAttribute('style', player_style_size);
+
+	var e = doc.getElementById('vwof_select_video_'+this.id);
+	if(e){
+	    i.setAttribute('src', e.options[e.selectedIndex].value);
+	}
+	else{
+	    var l=doc.getElementById('vwof_link_new_tab_'+this.id);
+	    i.setAttribute('src', l.href);
+	}
+
+	//replace the video selector with the frame
+	var video_selector_parent = this.parentNode;
+	video_selector_parent.replaceChild(i, this);
+    },
+    /**
+       Create the html code to embed the video selector in the document
+    */
+    create_video_selector:function(video_info, cw){
 	//set the player size depending on the location where embed it
 	var cstyle = cw.getComputedStyle(video_info['player']);
 	var doc = cw.document;
+
 	var h = cstyle.getPropertyValue("height");
 	var w = cstyle.getPropertyValue("width");
+	if(w == "0px")w="90vw";
+	if(h == "0px")h="90vh";
+
         var player_style_size = 'min-width:'+w+';min-height:'+h+';';
 	var player_style_image = '';
-	var player_id = 'vwof_' + Math.floor((Math.random() * 99));
+	video_selector_id = 'vwof_' + Math.floor((Math.random() * 99));
 
 	//add the player css if necessary
 	const style_id = 'vowf_player_style';
@@ -35,11 +64,12 @@ vwofPlayer = {
 	    this.add_style(doc, style_id);
 	}
 	
-	//Create the 'player' (div that call the frame with the video as src when clicked)
-	var player = doc.createElement('div');
-	player.setAttribute('class', 'vwof_player');
-	if(!player.id)player.setAttribute('id', player_id);  //identify the div player if the original element did not have an id
-	var player_onclick = "var i = document.createElement('iframe');i.setAttribute('class', 'vwof_frame');i.setAttribute('style', '"+player_style_size+"');";
+	//Create the 'video selector' (a div with detected videos links)
+	var video_selector = doc.createElement('div');
+	video_selector.setAttribute('class', 'vwof_player');
+	
+	//identify the div player if the original element did not have an id
+	if(!video_selector.id)video_selector.setAttribute('id', video_selector_id);
 	
 	//thumbnail
 	if(img = video_info['video_img']){
@@ -49,14 +79,14 @@ vwofPlayer = {
 	//vwof icon
 	var node_image = doc.createElement('img');
 	node_image.setAttribute('src', 'chrome://vwof/skin/video-icon.png');
-	player.appendChild(node_image);
+	video_selector.appendChild(node_image);
 
 	//select video format and quality
 	var prefered_index = 0;
 	
-	//if the number of video found for this player is only one, do not display the select and append a simple text
+	//if the number of video found for this video selector is only one, do
+	//not display the selectbox and append a simple text
 	if(video_info.videos.length == 1){
-	    player_onclick += "i.setAttribute('src', '"+video_info.videos[prefered_index].url+"');";
 	    var node_span = doc.createElement('span');
 	    node_span.setAttribute('class', 'vwof_video_info');
 	    var format = video_info.videos[0].format ? video_info.videos[0].format : '';
@@ -64,18 +94,24 @@ vwofPlayer = {
 	    var node_info = doc.createTextNode(format + ' ' + quality);
 	    
 	    node_span.appendChild(node_info);
-	    player.appendChild(node_span);
+	    video_selector.appendChild(node_span);
 	}
 	else{
 	    prefered_index = this.find_prefered_video(video_info.videos);
 
-	    player_onclick += "e=document.getElementById('vwof_select_video_"+player_id+"');i.setAttribute('src', e.options[e.selectedIndex].value);";
-	    
 	    var node_select = doc.createElement('select');
-	    node_select.setAttribute('id', 'vwof_select_video_'+ player_id);
+	    node_select.setAttribute('id', 'vwof_select_video_'+ video_selector_id);
 	    node_select.setAttribute('class', 'vwof_video_info');
-	    node_select.setAttribute('onclick', "event.stopPropagation();");
-	    node_select.setAttribute('onchange', 'document.getElementById(\'vwof_link_new_tab_'+player_id+'\').href=this.options[this.selectedIndex].value');
+	    node_select.addEventListener('click', function(event){
+		event.stopPropagation();
+	    });
+
+	    //update the open in a new tab link when the selectbox index changes
+	    node_select.addEventListener('change', function(event){
+		var doc = this.ownerDocument;
+		var link = doc.getElementById('vwof_link_new_tab_'+video_selector_id);
+		link.setAttribute('href', this.options[this.selectedIndex].value);
+	    });
 
 	    for(var i=0;i<video_info.videos.length;i++){
 		var node_option = doc.createElement('option');
@@ -91,23 +127,23 @@ vwofPlayer = {
 		
 		node_option.appendChild(node_option_content);
 		node_select.appendChild(node_option);
-		player.appendChild(node_select);
-		
+		video_selector.appendChild(node_select);
 	    }
 	}
 
-	player_onclick += 'var p = this.parentNode;p.replaceChild(i, this);';
-	player.setAttribute('onclick', player_onclick);
+	video_selector.addEventListener('click', this.video_selector_click);
 	
 	//open in a new tab link
 	var node_link_new_tab = doc.createElement('a');
-	node_link_new_tab.setAttribute('id', 'vwof_link_new_tab_'+player_id);
-	node_link_new_tab.setAttribute('onclick', 'event.stopPropagation();');
+	node_link_new_tab.setAttribute('id', 'vwof_link_new_tab_'+video_selector_id);
+	node_link_new_tab.addEventListener('click', function(event){
+	    event.stopPropagation();
+	});
 	node_link_new_tab.setAttribute('href', video_info.videos[prefered_index].url);
 	node_link_new_tab.setAttribute('target', '_blank');
 	var a_content_new_tab = doc.createTextNode('Open in a new tab');
 	node_link_new_tab.appendChild(a_content_new_tab);
-	player.appendChild(node_link_new_tab);
+	video_selector.appendChild(node_link_new_tab);
 
 	//display media source
 	if(video_info['source']){
@@ -115,13 +151,13 @@ vwofPlayer = {
 	    var node_source = doc.createTextNode('Powered by ' + video_info['source']);
 	    node_span.setAttribute('style', 'bottom:1px;left:1px;');
 	    node_span.appendChild(node_source);
-	    player.appendChild(node_span);
+	    video_selector.appendChild(node_span);
 	}
 
 	//apply style to the player
-	player.setAttribute('style', player_style_image + player_style_size);
+	video_selector.setAttribute('style', player_style_image + player_style_size);
 	
-	return player;
+	return video_selector;
     },
 
     /**

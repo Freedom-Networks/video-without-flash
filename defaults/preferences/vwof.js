f838534353bb7396998815744c1792b865cb5f23
olivier DREVET <odrevet@gmail.com>
2014-11-10 17:00:00 +0100

    removed obsolet module from module list

--- defaults/preferences/vwof.js
+++ defaults/preferences/vwof.js
@@ -1,4 +1,4 @@
 pref("extensions.vwof.activate_onload", true);
 pref("extensions.vwof.prefered_quality", 'medium');
 pref("extensions.vwof.prefered_format", 'webm');
-pref("extensions.vwof.modules", '{"HTML5":1, "springboard":1, "blip":1, "youtube":1, "dew":1, "ScreenWaveMedia":1, "dailymotion":1, "niconico":1, "FC2":1, "twitch":1}');
+pref("extensions.vwof.modules", '{"HTML5":1, "blip":1, "youtube":1, "dailymotion":1, "niconico":1, "FC2":1}');
